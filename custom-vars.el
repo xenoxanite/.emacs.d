53e5fc338852da914e000f56f88254b1a017eebb
samiulbasirfahim <xenoxanite@gmail.com>
2024-01-03 18:25:26 +0600

    updated

--- custom-vars.el
+++ custom-vars.el
@@ -4,8 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(initial-frame-alist '((fullscreen . maximized)))
+ '(org-agenda-files nil)
  '(package-selected-packages
-   '(treemacs-projectile hide-mode-line spaceline leetcode nix-mode lsp-ui which-key rainbow-delimiters diminish counsel-projectile counsel nerd-icons-ivy-rich ivy-rich ivy cape nerd-icons-corfu corfu diff-hl magit nerd-icons-ibuffer nerd-icons-dired org-superstar toc-org yasnippet-snippets lsp-mode projectile vterm-toggle vterm dashboard doom-modeline ewal-spacemacs-themes ewal treemacs-evil treemacs general evil-collection evil)))
+   '(org-roam-ui org-appear deft org-roam olivetti org-autolist xenops org-variable-pitch treemacs-projectile hide-mode-line spaceline leetcode nix-mode lsp-ui which-key rainbow-delimiters diminish counsel-projectile counsel nerd-icons-ivy-rich ivy-rich ivy cape nerd-icons-corfu corfu diff-hl magit nerd-icons-ibuffer nerd-icons-dired org-superstar toc-org yasnippet-snippets lsp-mode projectile vterm-toggle vterm dashboard doom-modeline ewal-spacemacs-themes ewal treemacs-evil treemacs general evil-collection evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
